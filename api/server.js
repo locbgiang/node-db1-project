1fc90148518830acf57a039d8bf597162c272ab3
unknown <locbgiang@gmail.com>
2020-05-11 17:45:04 -0700

    finished

--- api/server.js
+++ api/server.js
@@ -1,9 +1,19 @@
 const express = require("express");
 
+const AccountRouter = require('../accounts/account-router')
+
 const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', AccountRouter);
+
+server.get('/',(req,res)=>{
+    res.status(200).json({
+        api: "Up"
+    });
+})
 
 module.exports = server;
